6a2b78bcaed378351e75156d9aac0bfe559f6102
Simon McCartney <simon.mccartney@hpe.com>
2017-02-20 23:12:38 +0000

    Added molecule & travis for CI

---
+++ tests/test_default.py
@@ -0,0 +1,18 @@
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    '.molecule/ansible_inventory').get_hosts('all')
+
+
+def test_hosts_file(File):
+    f = File('/etc/hosts')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+
+
+def test_smokeping_package(Package):
+    smokeping = Package('smokeping')
+    assert smokeping.is_installed
+    assert smokeping.version.startswith("2.6")
